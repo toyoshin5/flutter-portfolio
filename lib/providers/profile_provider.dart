3681109975be3083e728f3bb7ad0034b3f90bbd8
豊田 真吾 <toyodasinngo@gmail.com>
2024-07-01 22:28:43 +0900

    Add dummy data

--- lib/providers/profile_provider.dart
+++ lib/providers/profile_provider.dart
@@ -66,6 +66,36 @@ class ProfileNotifier extends _$ProfileNotifier {
             BudgeType.swift,
           ], 
         ),
+        ProjectsModel(
+          title: 'ここがタイトル2',
+          date: '2021/08',
+          description: 'やったことやったことやったことやったこと',
+          imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfaWn5LsbQWyIAx7EdfX8px5jqW7H2wrPY6A&s',
+          budges: [
+            BudgeType.flutter,
+            BudgeType.swift,
+          ], 
+        ),
+        ProjectsModel(
+          title: 'ここがタイトルここがタイトルここがタイトルここがタイトルここがタイトル',
+          date: '2021/08',
+          description: 'やったことやったことやったことやったことやったことやったことやったことやったことやったことやったことやったことやったことやったことやったことやったことやったことやったことやったことやったことやったことやったことやったことやったことやったこと',
+          imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfaWn5LsbQWyIAx7EdfX8px5jqW7H2wrPY6A&s',
+          budges: [
+            BudgeType.flutter,
+            BudgeType.swift,
+          ], 
+        ),
+        ProjectsModel(
+          title: 'ここがタイトル2',
+          date: '2021/08',
+          description: 'やったことやったことやったことやったこと',
+          imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfaWn5LsbQWyIAx7EdfX8px5jqW7H2wrPY6A&s',
+          budges: [
+            BudgeType.flutter,
+            BudgeType.swift,
+          ], 
+        ),
       ],
       skills: [
         SkillsModel(

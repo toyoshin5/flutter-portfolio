e90765f23dd5ccb337d26351dd16ba8d9177487f
豊田 真吾 <toyodasinngo@gmail.com>
2024-07-29 10:27:11 +0900

    refactor

--- lib/notifier/cook_path_notifier.dart
+++ lib/notifier/cook_path_notifier.dart
@@ -9,10 +9,8 @@ part 'cook_path_notifier.g.dart';
 class CookPathNotifier extends _$CookPathNotifier {
   @override
   Future<List<String>> build() async {
-    // >> To get paths you need these 2 lines
       final manifestContent = await rootBundle.loadString('AssetManifest.json');
       final Map<String, dynamic> manifestMap = json.decode(manifestContent);
-      // >> To get paths you need these 2 lines
       final imagePaths = manifestMap.keys
           .where((String key) => key.contains('images/cook/'))
           .where((String key) => key.contains('.jpg') || key.contains('.jpeg'))
